b686e369373decb4a5fef2b8e46fab0f3def4555
Роман <golrk0506@gmail.com>
2024-09-21 15:31:44 +0400

    -1

--- Classes.py
+++ Classes.py
@@ -1,44 +1,89 @@
 import datetime
+
 class Feature:
-    def init(self, last_feature_id, app):
+    def __init__(self, last_feature_id: int, app: 'App'):
         self.feature_id = last_feature_id + 1
         self.app_id = app.app_id
-        self.created_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+
+    @property
+    def feature_name(self):
+        return self._feature_name
+
+    @feature_name.setter
+    def feature_name(self, value):
+        if not isinstance(value, list):
+            raise TypeError("Expected a list of characters")
+        self._feature_name = value
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Expected a string")
+        self._description = value
+
+    @property
+    def created_at(self):
+        return self._created_at
+
+    @created_at.setter
+    def created_at(self, value):
+        if not isinstance(value, datetime):
+            raise TypeError("Expected a date/time object")
+        self._created_at = value
+
+    @property
+    def updated_at(self):
+        return self._updated_at
+
+    @updated_at.setter
+    def updated_at(self, value):
+        if not isinstance(value, datetime):
+            raise TypeError("Expected a date/time object")
+        self._updated_at = value
 
     def start_voting(self):
-        pass
+        votes = Votes(self)
 
 
 class App:
-    def init(self):
-        self.app_id = None
-        self.app_name = None
+    def __init__(self, app_id: int, app_name: int):
+        self.app_id = app_id
+        self.app_name = app_name
 
 
 class Logs:
-    def init(self, log_id, user_id, action, created_at):
+    def __init__(self, log_id: int, user_id: int, action: list, created_at: datetime):
         self.log_id = log_id
         self.user_id = user_id
         self.action = action
         self.created_at = created_at
 
-
 class User:
-    def init(self, email):
-        self.user_id = sum(ord(c) for c in email)
-
+    def __init__(self, email: list):
+        try:
+            self.user_id = sum([int(c) for c in email])
+        except ValueError as e:
+            raise ValueError('Email should be a valid integer sequence') from e
+        else:
+            self.email = email
+            self.created_at = datetime.now()
+            self.app_id = None
 
 class Votes:
-    def init(self, feature):
-        self.vote_id = None
-        self.user_id = None
+    def __init__(self, feature: 'Feature', user_id: int):
+        self.vote_id = 0
+        self.user_id = user_id
         self.feature_id = feature.feature_id
-        self.created_at = datetime.datetime.now()
+        self.created_at = datetime.now()
 
 
 class Results:
-
-    def init(self, result_id, feature_id, total_votes, positive_votes, negative_votes, last_updated):
+    def __init__(self, result_id: int, feature_id: int, total_votes: int, positive_votes: int, negative_votes: int, last_updated: datetime):
         self.result_id = result_id
         self.feature_id = feature_id
         self.total_votes = total_votes
