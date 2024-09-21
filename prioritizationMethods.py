9878c461567aebffcd6d5a84918b34978d374571
Egor <89373012+EgorEngineer@users.noreply.github.com>
2024-09-21 18:23:11 +0400

    -добавил библиотеки графиков

--- prioritizationMethods.py
+++ prioritizationMethods.py
@@ -2,15 +2,13 @@ class RICE:
     """
     Класс для приоритизации RICE (Reach, Impact, Confidence, Effort).
     """
-
     def __init__(self, reach: float, impact: float, confidence: float, effort: float):
         """
         Инициализация параметров RICE.
-
-        :param reach: Сколько людей или сегментов затронет проект.
-        :param impact: Насколько сильно повлияет проект (оценка от 1 до 5).
-        :param confidence: Уверенность в оценках reach и impact (процент от 0 до 100).
-        :param effort: Количество усилий (в человеко-месяцах или других единицах), необходимых для реализации проекта.
+        reach: Сколько людей или сегментов затронет проект.
+        impact: Насколько сильно повлияет проект (оценка от 1 до 5).
+        confidence: Уверенность в оценках reach и impact (процент от 0 до 100).
+        effort: Количество усилий (в человеко-месяцах или других единицах), необходимых для реализации проекта.
         """
         self.reach = reach
         self.impact = impact
@@ -20,8 +18,6 @@ class RICE:
     def calculate_rice_score(self) -> float:
         """
         Рассчитывает RICE score.
-
-        :return: RICE score
         """
         rice_score = (self.reach * self.impact * self.confidence) / self.effort
         return rice_score
@@ -31,14 +27,12 @@ class ICE:
     """
     Класс для приоритизации ICE (Impact, Confidence, Effort).
     """
-
     def __init__(self, impact: float, confidence: float, effort: float):
         """
         Инициализация параметров ICE.
-
-        :param impact: Насколько сильно повлияет проект (оценка от 1 до 10).
-        :param confidence: Уверенность в оценках impact (процент от 0 до 100).
-        :param effort: Количество усилий (в человеко-месяцах или других единицах), необходимых для реализации проекта.
+        impact: Насколько сильно повлияет проект (оценка от 1 до 10).
+        confidence: Уверенность в оценках impact (процент от 0 до 100).
+        effort: Количество усилий (в человеко-месяцах или других единицах), необходимых для реализации проекта.
         """
         self.impact = impact
         self.confidence = confidence / 100  # Преобразуем процент в десятичную дробь
@@ -47,8 +41,6 @@ class ICE:
     def calculate_ice_score(self) -> float:
         """
         Рассчитывает ICE score.
-
-        :return: ICE score
         """
         ice_score = (self.impact * self.confidence) / self.effort
         return ice_score
@@ -58,7 +50,6 @@ class Kano:
     """
     Класс для анализа приоритизации на основе модели Kano.
     """
-
     def __init__(self):
         """
         Инициализация модели Kano.
@@ -74,10 +65,9 @@ class Kano:
     def add_feature(self, feature_name: str, customer_satisfaction: int, functionality: int):
         """
         Добавление функциональности на основе анализа Kano.
-
-        :param feature_name: Название функциональности.
-        :param customer_satisfaction: Оценка удовлетворения пользователей (от -1 до 1, где -1 — недовольны, 0 — индифферентно, 1 — довольны).
-        :param functionality: Оценка функциональности (от -1 до 1, где -1 — отрицательно, 0 — нейтрально, 1 — положительно).
+        feature_name: Название функциональности.
+        customer_satisfaction: Оценка удовлетворения пользователей (от -1 до 1, где -1 — недовольны, 0 — индифферентно, 1 — довольны).
+        functionality: Оценка функциональности (от -1 до 1, где -1 — отрицательно, 0 — нейтрально, 1 — положительно).
         """
         if customer_satisfaction == 1 and functionality == 1:
             self.kano_categories['One-dimensional'].append(feature_name)
@@ -93,8 +83,6 @@ class Kano:
     def get_features_by_category(self, category: str) -> list:
         """
         Возвращает список функциональностей по категории Kano.
-
-        :param category: Категория (Must-have, One-dimensional, Attractive, Indifferent, Reverse).
-        :return: Список функциональностей в данной категории.
+        category: Категория (Must-have, One-dimensional, Attractive, Indifferent, Reverse).
         """
         return self.kano_categories.get(category, [])
